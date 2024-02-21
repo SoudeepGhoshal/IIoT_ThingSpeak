7c08ae85ea6a9312f49e82eb0610ed8f8808e5a8
Soudeep Ghoshal <49481277+SoudeepGhoshal@users.noreply.github.com>
2024-02-21 17:02:05 +0530

    Add files via upload

---
+++ Exp3_DHT11.ino
@@ -0,0 +1,32 @@
+#include <DHT.h>
+
+#define DHTPIN 27 // GPIO pin where the DHT11 is connected
+#define DHTTYPE DHT11 // Type of the sensor
+
+DHT dht(DHTPIN, DHTTYPE);
+
+void setup() {
+  Serial.begin(9600);
+  dht.begin();
+}
+
+void loop() {
+  delay(2000); // Wait for 2 seconds between measurements
+
+  float humidity = dht.readHumidity();
+  float temperature = dht.readTemperature();
+
+  // Check if any reads failed and exit early (to try again).
+  if (isnan(humidity) || isnan(temperature)) {
+    Serial.println("Failed to read from DHT sensor!");
+    return;
+  }
+
+  // Print the temperature and humidity values.
+  Serial.print("Humidity: ");
+  Serial.print(humidity);
+  Serial.print("%\t");
+  Serial.print("Temperature: ");
+  Serial.print(temperature);
+  Serial.println("°C");
+}
