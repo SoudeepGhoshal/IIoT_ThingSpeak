51bf0629fc925f579a4d3374a9bad058754965ae
Soudeep Ghoshal <49481277+SoudeepGhoshal@users.noreply.github.com>
2024-02-21 17:07:07 +0530

    Add files via upload

---
+++ Exp1_LED Blink.ino
@@ -0,0 +1,17 @@
+const int ledPin=19;
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(ledPin,OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(ledPin,HIGH);
+  Serial.println("ON");
+  delay(1000);
+  digitalWrite(ledPin,LOW);
+  Serial.println("OFF");
+  delay(1000);
+  }
